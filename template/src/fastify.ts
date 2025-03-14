a7633225961d7f86ef0834c0e30192950a0eed93
YanluPei <peiyanlu@vip.qq.com>
2025-03-14 22:05:11 +0800

    feat: add SWC options

--- template/src/fastify.ts
+++ template/src/fastify.ts
@@ -3,14 +3,14 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { AppModule } from './app.module'
 
 
-const bootstrap = async () => {
+const bootstrap = async (port: number | string) => {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   )
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(port)
   
-  console.log(`\nApp listening on: http://127.0.0.1:3000`)
+  console.log(`\nApp listening on: http://127.0.0.1:${ port }`)
 }
 
-bootstrap().catch(console.error)
+bootstrap(process.env.PORT ?? 3000).catch(console.error)
